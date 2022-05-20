181884fe1b95a750c695cb38803cf5ae6f0750e9
arthitsendev <arthitsen@icloud.com>
2022-05-20 12:20:16 -0400

    All programs update from Kali Linux.

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -2,30 +2,30 @@ import java.util.*;
 class BinarySearch {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int a[]=new int[10],i,l,u,m,n,f=0;
+        int i,a[]=new int[10],n,l=0,u,m,p=0,e=0,f=0;
         System.out.print("Enter 10 numbers=");
         for(i=0;i<10;i++)
             a[i]=sc.nextInt();
-        l=0;
-        u=9;
         System.out.print("Enter a number to search=");
         n=sc.nextInt();
+        l=0;
+        u=9;
         while(l<=u) {
             m=(l+u)/2;
-            if(n<a[m]) {
-                u=m-1;
-            }
-            else if(n>a[m]) {
+            if(a[m]<n)
                 l=m+1;
-            }
+            else if(a[m]>n)
+                u=m-1;
             else {
                 f=1;
+                p=m;
+                e=a[m];
                 break;
             }
         }
         if(f==1)
-            System.out.print("Search successful");
+            System.out.print("Element= "+e+" "+"Position= "+p);
         else
-            System.out.print("Search unsuccessful");
+            System.out.print("Search element not found");
     }
 }
